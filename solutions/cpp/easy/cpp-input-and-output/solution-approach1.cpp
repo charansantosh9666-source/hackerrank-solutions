61c60e23f481b64a02fee3782d06a273c3deb246
Charan santosh <25Q91A05AU@mrce.in>
2026-08-20 11:49:40 +0530

    ✅ Input and Output [cpp] — approach1

---
+++ solutions/cpp/easy/cpp-input-and-output/solution-approach1.cpp
@@ -0,0 +1,26 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/cpp-input-and-output/problem?isFullScreen=true
+// Problem     Input and Output
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    cpp
+// Status      Accepted
+// Submitted   2026-08-20, 11:49 a.m.
+// ──────────────────────────────────────────────────
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int a,b,c;
+    cin>>a>>b>>c;
+    cout<<a+b+c;
+    return 0;
+}

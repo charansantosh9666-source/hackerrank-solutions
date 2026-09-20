6abd5f51f330240320a8748fb50fe87bcfcb3c92
Charan santosh <25Q91A05AU@mrce.in>
2026-09-20 21:21:05 +0530

    ✅ Python If-Else [python3] — approach1

---
+++ solutions/python3/easy/py-if-else/solution-approach1.py
@@ -0,0 +1,31 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/py-if-else/problem?isFullScreen=true
+# Problem     Python If-Else
+# Difficulty  Easy
+# Subdomain   Introduction
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-20, 09:21 p.m.
+# ──────────────────────────────────────────────────
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n%2!=0:
+        print("Weird")
+    elif n%2==0 and n>=2 and n<=5:
+        print("Not Weird")
+    elif n%2==0 and n>=6 and n<=20:
+        print("Weird")
+    else:
+        print("Not Weird")

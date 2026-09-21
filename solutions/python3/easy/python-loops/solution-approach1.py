865dab01c045b45bb1730670f857fb80cb8c2b6e
Charan santosh <25Q91A05AU@mrce.in>
2026-09-21 07:14:29 +0530

    ✅ Loops [python3] — approach1

---
+++ solutions/python3/easy/python-loops/solution-approach1.py
@@ -0,0 +1,15 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true
+# Problem     Loops
+# Difficulty  Easy
+# Subdomain   Introduction
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-21, 07:14 a.m.
+# ──────────────────────────────────────────────────
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(0,n):
+        print(i*i)
